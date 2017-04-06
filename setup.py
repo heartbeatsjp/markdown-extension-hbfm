a8b69faf1771a03386930c3fe36a28b194a3edc5
Toshiaki Baba <toshiaki@netmark.jp>
2017-04-06 11:38:46 +0900

    add setup and requirements

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="hbfm",
+    version="0.0.1",
+    description="HEARTBEATS Frafored Markdown extensions",
+    author="Toshiaki Baba",
+    author_email="toshiaki@netmark.jp",
+    url="https://github.com/heartbeatsjp/markdown-extension-hbfm",
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+    ],
+    packages=["hbfm"],
+)
