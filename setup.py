c6c6cfcb5f5b2280d7191eb28db7a705e1987edf
Toshiaki Baba <toshiaki@netmark.jp>
2018-03-01 16:33:55 +0900

    bump up to 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="hbfm",
-    version="0.0.3",
+    version="1.0.0",
     description="HEARTBEATS Flavored Markdown extensions",
     author="Toshiaki Baba",
     author_email="toshiaki@netmark.jp",
@@ -10,6 +10,7 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
     ],
     packages=["hbfm"],
 )
